0d258d46ee15cd00cdd1674b69b2183f2e84c3ad
MustafaAlasadi99 <malasadi576@gmail.com>
2022-07-09 16:09:52 -0500

    Updates

---
+++ precache-manifest.0350545b9a33233a8d6c66bad86ddce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8fba373403cb893c31769c7187ae12",
+    "url": "/YoutubeClone/index.html"
+  },
+  {
+    "revision": "d2c2dd4e9f0e95dca2d7",
+    "url": "/YoutubeClone/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "0e1ac9484664ec9a9194",
+    "url": "/YoutubeClone/static/css/main.a1ead8a2.chunk.css"
+  },
+  {
+    "revision": "d2c2dd4e9f0e95dca2d7",
+    "url": "/YoutubeClone/static/js/2.62cce651.chunk.js"
+  },
+  {
+    "revision": "0e1ac9484664ec9a9194",
+    "url": "/YoutubeClone/static/js/main.2a30f59a.chunk.js"
+  },
+  {
+    "revision": "5c46016c73efa55d736d",
+    "url": "/YoutubeClone/static/js/runtime~main.eeba27c1.js"
+  }
+]);
